5b50ec4d22dbc795f9adaf26316daa733a2de6eb
niickyds <ntsd1158@gmail.com>
2023-11-27 14:19:41 -0600

    created app folder

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <body>
+      <header></header>
+      <main></main>
+      <footer></footer>
+      <modalWithForm />
+      <ItemModal />
+    </body>
+  );
+}
+
+export default App;
