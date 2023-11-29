b8247619b3147eb10f9b7d4c2fb9ea5b1b137827
niickyds <ntsd1158@gmail.com>
2023-11-29 10:47:55 -0600

    modal form with radio button setup

--- src/App.js
+++ src/App.js
@@ -7,12 +7,43 @@ import ModalWithForm from "./ModalWithForm/ModalWithForm";
 
 function App() {
   const weatherTemp = "75°F";
+  // const [activeModal, setActiveModal] = useState("");
   return (
     <div>
       <Header />
       <Main weatherTemp={weatherTemp} />
       <Footer />
-      <ModalWithForm title="New Garment">children</ModalWithForm>
+      <ModalWithForm title="New Garment">
+        <label>
+          Name
+          <input
+            type="text"
+            name="name"
+            minlenth="1"
+            maxLength="30"
+            placeholder="Name"
+          />
+        </label>
+        <label>
+          Image
+          <input type="url" name="link" minlenth="1" placeholder="Image URL" />
+        </label>
+        <p>Select the weather type:</p>
+        <div>
+          <div>
+            <input type="radio" id="hot" value="hot" />
+            <label>Hot</label>
+          </div>
+          <div>
+            <input type="radio" id="warm" value="warm" />
+            <label>Warm</label>
+          </div>
+          <div>
+            <input type="radio" id="cold" value="cold" />
+            <label>Cold</label>
+          </div>
+        </div>
+      </ModalWithForm>
     </div>
   );
 }
