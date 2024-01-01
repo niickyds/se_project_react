a2575606e232e9d15d26823721264024cc3a9e2b
niickyds <ntsd1158@gmail.com>
2024-01-01 14:13:29 -0600

    double profile text fix

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -7,16 +7,6 @@ const Profile = ({ onCreate, clothingItems, onSelectCard }) => {
     <div className="profile">
       <SideBar />
       <div className="profile__items-container">
-        <div className="profile__items-text">
-          Your Items
-          <button
-            className="profile__add-button"
-            onClick={onCreate}
-            type="button"
-          >
-            + Add new
-          </button>
-        </div>
         <ClothesSection
           clothingItems={clothingItems}
           onSelectCard={onSelectCard}
