31b8197b370ad9dee386ff5186f05639b647afa0
niickyds <ntsd1158@gmail.com>
2024-05-29 05:22:38 -0500

    sidebar button/css update

--- src/components/Profile/SideBar.jsx
+++ src/components/Profile/SideBar.jsx
@@ -5,13 +5,25 @@ const SideBar = () => {
   const currentUser = React.useContext(CurrentUserContext);
 
   return (
-    <div className="profile__logo">
-      <img
-        className="profile__avatar-logo"
-        src={currentUser.avatar}
-        alt="Avatar"
-      />
-      <p className="profile__user-name">{currentUser.name}</p>
+    <div className="profile__sidebar">
+      <div className="profile__logo">
+        <img
+          className="profile__avatar-logo"
+          src={currentUser.avatar}
+          alt="Avatar"
+        />
+        <p className="profile__user-name">{currentUser.name}</p>
+      </div>
+      <div className="profile__buttons">
+        <button type="button" className="profile__button">
+          {" "}
+          Change profile data
+        </button>
+        <button type="button" className="profile__button">
+          {" "}
+          Log out
+        </button>
+      </div>
     </div>
   );
 };
